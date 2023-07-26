5f45656c2ef763b3a598d67c1955678b2388c8cc
changxv <48442578@qq.com>
2023-07-26 19:28:03 +0800

    update the locations of the paintings

--- Assets/Scripts/Demo/Museum.cs
+++ Assets/Scripts/Demo/Museum.cs
@@ -27,7 +27,25 @@ public class Museum : MonoBehaviour
         cameraRecorder.AddCameraData("Camera.013", new Vector3(-52.5f, 16.5f, -75.3f), Quaternion.Euler(0f, 180f, 0f));
         cameraRecorder.AddCameraData("Camera.014", new Vector3(-34.5f, 16.5f, -59f), Quaternion.Euler(0f, 120f, 0f));
         cameraRecorder.AddCameraData("Camera.015", new Vector3(-69.5f, 16.5f, 74.35f), Quaternion.Euler(0f, 180f, 0f));
-    }
+        cameraRecorder.AddCameraData("Camera.016", new Vector3(-277f, 16.5f, 25f), Quaternion.Euler(0f, -90f, 0f));
+        cameraRecorder.AddCameraData("Camera.017", new Vector3(-277f, 16.5f, -26f), Quaternion.Euler(0f, -90f, 0f));
+        cameraRecorder.AddCameraData("Camera.018", new Vector3(-23f, 16.5f, -26f), Quaternion.Euler(0f, 90f, 0f));
+        cameraRecorder.AddCameraData("Camera.019", new Vector3(-23f, 16.5f, 26f), Quaternion.Euler(0f, 90f, 0f));
+        cameraRecorder.AddCameraData("Camera.020", new Vector3(-168f, 16.5f, 0f), Quaternion.Euler(0f, 270f, 0f));
+        cameraRecorder.AddCameraData("Camera.021", new Vector3(-104f, 16.5f, 42f), Quaternion.Euler(0f, 180f, 0f));
+        cameraRecorder.AddCameraData("Camera.022", new Vector3(-78f, 16.5f, 42f), Quaternion.Euler(0f, 180f, 0f));
+        cameraRecorder.AddCameraData("Camera.023", new Vector3(-196f, 16.5f, -42f), Quaternion.Euler(0f, 0f, 0f));
+        cameraRecorder.AddCameraData("Camera.024", new Vector3(-222f, 16.5f, -42f), Quaternion.Euler(0f, 0f, 0f));
+        cameraRecorder.AddCameraData("Camera.025", new Vector3(-197f, 16.5f, 42f), Quaternion.Euler(0f, 180f, 0f));
+        cameraRecorder.AddCameraData("Camera.026", new Vector3(-223f, 16.5f, 42f), Quaternion.Euler(0f, 180f, 0f));
+        cameraRecorder.AddCameraData("Camera.027", new Vector3(-102.5f, 16.5f, -42f), Quaternion.Euler(0f, 0f, 0f));
+        cameraRecorder.AddCameraData("Camera.028", new Vector3(-77.5f, 16.5f, -42f), Quaternion.Euler(0f, 0f, 0f));
+        cameraRecorder.AddCameraData("Camera.029", new Vector3(-253f, 16.5f, 0f), Quaternion.Euler(0f, 90f, 0f));
+        cameraRecorder.AddCameraData("Camera.030", new Vector3(-141f, 19f, 173f), Quaternion.Euler(0f, 90f, 0f));
+        cameraRecorder.AddCameraData("Camera.031", new Vector3(-159f, 19f, 172f), Quaternion.Euler(0f, 270f, 0f));
+        cameraRecorder.AddCameraData("Camera.032", new Vector3(-133f, 20f, 241f), Quaternion.Euler(0f, 270f, 0f));
+        cameraRecorder.AddCameraData("Camera.033", new Vector3(-168f, 20f, 241f), Quaternion.Euler(0f, 90f, 0f));
+  }
 
     private void Update()
     {
